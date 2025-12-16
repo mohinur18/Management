cf9cde0033a8270dc08713d87d99b45ebf134cf6
mohinur18 <muhammadjonovamohinur36@gmail.com>
2025-12-16 20:43:27 +0500

    Client

---
+++ Management.Clientt/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Management.Application;
+
+namespace Management.Clientt
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            var studentService = new StudentService();
+            studentService.AddStudent("Mohinur", "Muhammadjonova");
+        }
+    }
+}
--- Management.Infrastructure/Data/DbContext.cs
+++ Management.Infrastructure/Data/DbContext.cs
@@ -9,6 +9,12 @@ namespace Management.Infrastructure.Data
 {
     public class DbContext
     {
+        public DbContext()
+    {
+        this.Students = new Student[12];
+    }
+   
         public Student[] Students { get; set; }
+        public int StudentCount { get; set; } = 0;
     }
 }
