93dc5febb2d9efefb7b68bb44fcfe8b33460a688
mohinur18 <muhammadjonovamohinur36@gmail.com>
2025-12-16 18:56:03 +0500

    Array added

---
+++ Management.Application/StudentService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Management.Domain.Models;
+using Management.Infrastructure.Data;
+
+namespace Management.Application
+{
+    public class StudentService
+    {
+        public DbContext DbContext { get; set; }
+        public StudentService()
+        {
+            this .DbContext = new DbContext();
+        }
+        public void AddStudent(string firstName, string lastName)
+        {
+            Student newStudent = new Student
+            {
+                Id = new Random().Next(1, 1000).ToString(),
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            this.DbContext.Students[0] = newStudent;
+        }
+
+    }
+}
