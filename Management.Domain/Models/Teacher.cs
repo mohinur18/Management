20e6bcb916aed1dd1e35dab80ea806a5ba3fe206
mohinur18 <muhammadjonovamohinur36@gmail.com>
2025-12-16 18:18:01 +0500

    Infrastructure

--- Management.Domain/Models/Teacher.cs
+++ Management.Domain/Models/Teacher.cs
@@ -8,6 +8,8 @@ namespace Management.Domain.Models
 {
     public class Teacher
     {
-
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
---
+++ Management.Infrastructure/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Management.Domain.Models;
+
+namespace Management.Infrastructure.Data
+{
+    public class DbContext
+    {
+        public Student[] Students { get; set; }
+    }
+}
