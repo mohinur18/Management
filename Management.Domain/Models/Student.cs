f712991a44da42838d5ab569e95c3e7cd5e02f89
mohinur18 <muhammadjonovamohinur36@gmail.com>
2025-12-16 17:55:19 +0500

    Add project files.

---
+++ Management.Domain/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Management.Domain.Models
+{
+    public class Student
+    {
+        public string Id {  get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
